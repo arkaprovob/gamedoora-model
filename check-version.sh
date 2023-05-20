e006bbd0c76c8ac88ed9373e4d0185d99b6b47ad
Arko <47105136+arkaprovob@users.noreply.github.com>
2023-05-20 22:33:33 +0530

    Add execute permissions to script

--- check-version.sh
+++ check-version.sh
@@ -5,8 +5,9 @@ URL=https://maven.pkg.github.com/arkaprovob/gamedoora-model/com/gamedoora/gamedo
 STATUS_CODE=$(curl --silent --output /dev/null --write-out "%{http_code}\n" -u ${USER}:${PAT} $URL)
 
 if [ $STATUS_CODE -eq 200 ]; then
+  echo "::set-output name=exists::true"
   echo "Version $VERSION already exists."
-  exit 1
 else
-  echo "Version $VERSION does not exist, proceeding with the deployment."
+  echo "::set-output name=exists::false"
+  echo "Version $VERSION does not exist, proceeding with the artifact upload."
 fi
